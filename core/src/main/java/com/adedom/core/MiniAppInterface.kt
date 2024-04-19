77513c948fa596014bb1d7a82734df5ab7259309
Thanathip <thanathip@CH7.com>
2024-04-19 16:37:35 +0700

    delete

---
+++ core/src/main/java/com/adedom/core/MiniAppInterface.kt
@@ -0,0 +1,5 @@
+package com.adedom.core
+
+interface MiniAppInterface {
+    fun open(path: String)
+}
---
+++ lifecycle/src/main/java/com/example/lifecycle/LifeCycle.kt
@@ -0,0 +1,10 @@
+package com.example.lifecycle
+
+import android.content.Intent
+import com.adedom.core.MiniAppInterface
+
+class LifeCycle: MiniAppInterface {
+    override fun open(path: String) {
+
+    }
+}
